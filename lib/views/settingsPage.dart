7340cfbf190ba0402c89a2f8ca41ab287d38a771
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-11-18 11:01:29 -0400

    Add theme selection option in settings

--- lib/views/settingsPage.dart
+++ lib/views/settingsPage.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vibe_project/theme/customTheme.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  // Variável para armazenar o tema selecionado
+  ThemeMode? _selectedThemeMode;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedThemeMode = CustomTheme.themeMode;
+  }
+
+  void _changeTheme(ThemeMode? value) {
+    setState(() {
+      _selectedThemeMode = value;
+      Get.changeThemeMode(value ?? ThemeMode.system);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +36,29 @@ class SettingsPage extends StatelessWidget {
       body: Column(
         children: [
           Center(
-            child: Text('Settings!'),
+            child: Text('Configurações de Tema'),
+          ),
+          // Dropdown para seleção do tema
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: DropdownButton<ThemeMode>(
+              value: _selectedThemeMode,
+              onChanged: _changeTheme,
+              items: [
+                DropdownMenuItem(
+                  value: ThemeMode.system,
+                  child: Text("Padrão do Sistema"),
+                ),
+                DropdownMenuItem(
+                  value: ThemeMode.light,
+                  child: Text("Modo Claro"),
+                ),
+                DropdownMenuItem(
+                  value: ThemeMode.dark,
+                  child: Text("Modo Escuro"),
+                ),
+              ],
+            ),
           ),
         ],
       ),
